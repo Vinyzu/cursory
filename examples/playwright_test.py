4d3926d33088aee2f63954fbe42c95f367090669
Vinyzu <50874994+Vinyzu@users.noreply.github.com>
2026-04-08 14:30:09 +0200

    Update v1.1.0

--- examples/playwright_test.py
+++ examples/playwright_test.py
@@ -6,7 +6,19 @@ import time
 
 from cursory import generate_trajectory
 from playwright.sync_api import Playwright, sync_playwright
+from selenium_driverless.utils.utils import sel_driverless_path  # type: ignore[import-untyped]
 
+def high_precision_sleep(duration):
+    start_time = time.perf_counter()
+    while True:
+        elapsed_time = time.perf_counter() - start_time
+        remaining_time = duration - elapsed_time
+        if remaining_time <= 0:
+            break
+        if remaining_time > 0.02:  # Sleep for 5ms if remaining time is greater
+            time.sleep(max(remaining_time/2, 0.0001))  # Sleep for the remaining time or minimum sleep interval
+        else:
+            pass
 
 def run(playwright: Playwright) -> None:
     """Run a sample test using Playwright."""
@@ -15,15 +27,17 @@ def run(playwright: Playwright) -> None:
 
     page.goto("https://google.com/blank.html")
     # Inject mouse tracking script
-    with open("show_mouse.html") as f:
-        page.set_content(f.read())
+    page.add_script_tag(path=sel_driverless_path() + "files/js/show_mousemove.js")
 
     end_point = (0, 0)
+    total_points = 0
+    timeout_points = 0
+
     for _ in range(10):
         # Generate a random trajectory
         start_point = end_point
         end_point = (random.randint(0, 1080), random.randint(0, 720))
-        trajectory_points, timings = generate_trajectory(start_point, end_point, frequency_randomizer=0, frequency=100)
+        trajectory_points, timings = generate_trajectory(start_point, end_point, frequency_randomizer=0, frequency=60)
         # Print Frequency
         avg_freq = 1000 / (timings[-1] - timings[0]) * len(timings)
         print(f"Average Frequency: {avg_freq:.2f} Hz")
@@ -39,12 +53,18 @@ def run(playwright: Playwright) -> None:
             pause_time = timing - (timings[i - 1] if i > 0 else 0)
             if delay_time > pause_time:
                 print(f"[WARNING]: Delay ({delay_time:.2f} ms) is greater than timing pause ({pause_time} ms)")
-            page.wait_for_timeout(pause_time - delay_time)
+                timeout_points += 1
+            total_points += 1
+
+            sleep_delay = max(((pause_time - delay_time) / 1000), 0)
+            # page.wait_for_timeout(pause_time - delay_time)
+            high_precision_sleep(sleep_delay)
 
         # Click at the end point
         page.mouse.click(end_point[0], end_point[1])
         page.wait_for_timeout(1000)
 
+    print(f"Total Points: {total_points}, Timeout Points: {timeout_points}, Percentage: {(timeout_points/total_points)*100:.2f}%")
     browser.close()
 
 
